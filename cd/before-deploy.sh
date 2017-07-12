9c430706edac69c62026b4dd019941d509edc365
Chris Herrera <chris.herrera@hashmapinc.com>
2017-07-12 17:21:08 -0500

    Removing old cert

--- cd/before-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_f1181ebdf3ca_key -iv $encrypted_f1181ebdf3ca_iv -in cd/codesign.asc.enc -out cd/codesign.asc -d
-    gpg --fast-import cd/codesign.asc
-fi
