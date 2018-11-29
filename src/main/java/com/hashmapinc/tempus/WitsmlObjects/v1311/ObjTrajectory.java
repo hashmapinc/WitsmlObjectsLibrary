2dda5bcda9eeab1476e531d54e992e1f11bb3aae
Randy Pitcher <randypitcherii@gmail.com>
2018-11-29 16:01:47 -0600

    extended AWO in 1311 trajectory

--- src/main/java/com/hashmapinc/tempus/WitsmlObjects/v1311/ObjTrajectory.java
+++ src/main/java/com/hashmapinc/tempus/WitsmlObjects/v1311/ObjTrajectory.java
@@ -11,6 +11,8 @@ import javax.xml.bind.annotation.XmlType;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 import com.hashmapinc.tempus.WitsmlObjects.AbstractWitsmlObject;
+import com.hashmapinc.tempus.WitsmlObjects.Util.WitsmlMarshal;
+import com.hashmapinc.tempus.WitsmlObjects.Util.WitsmlVersionTransformer;
 
 /**
  * <p>Java class for obj_trajectory complex type.
@@ -64,7 +66,7 @@ import com.hashmapinc.tempus.WitsmlObjects.AbstractWitsmlObject;
     "commonData",
     "customData"
 })
-public class ObjTrajectory {
+public class ObjTrajectory extends AbstractWitsmlObject {
 
     @XmlElement(required = true)
     protected String nameWell;
@@ -165,6 +167,7 @@ public class ObjTrajectory {
      *     {@link String }
      *     
      */
+    @Override
     public String getName() {
         return name;
     }
@@ -698,6 +701,7 @@ public class ObjTrajectory {
      *     {@link String }
      *     
      */
+    @Override
     public String getUid() {
         return uid;
     }
@@ -712,6 +716,65 @@ public class ObjTrajectory {
      */
     public void setUid(String value) {
         this.uid = value;
+    }
+
+    /**
+     * get the version as a string
+     */
+    @Override
+    public String getVersion() {
+        return "1.3.1.1";
+    }
+
+    /**
+     * Gets this object as an xml string in the requested version format.
+     *
+     * @param version - WITSML version to serialize to
+     * @return xml - string value holding the xml string
+     */
+    @Override
+    public String getXMLString(String version) {
+        try {
+            ObjTrajectorys trajectorys = new ObjTrajectorys();
+            trajectorys.addTrajectory(this);
+            String xml1311 = WitsmlMarshal.serialize(trajectorys);
+            if ("1.4.1.1".equals(version)) {
+                return (new WitsmlVersionTransformer()).convertVersion(xml1311);
+            } else if ("1.3.1.1".equals(version)) {
+                return xml1311;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Gets this object as an json string in the requested version format.
+     *
+     * @param version - WITSML version to serialize to
+     * @return json - String value holding the json string
+     */
+    @Override
+    public String getJSONString(String version) {
+        try {
+            if ("1.4.1.1".equals(version)) {
+                // convert to 1411 pojo and parse as json
+                String xml1411 = this.getXMLString("1.4.1.1");
+                com.hashmapinc.tempus.WitsmlObjects.v1411.ObjTrajectorys trajectorys = WitsmlMarshal.deserialize(xml1411,
+                        com.hashmapinc.tempus.WitsmlObjects.v1411.ObjTrajectorys.class);
+                return WitsmlMarshal.serializeToJSON(trajectorys.getTrajectory().get(0));
+            } else if ("1.3.1.1".equals(version)) {
+                return WitsmlMarshal.serializeToJSON(this);
+            } else {
+                return null; // unsupported version
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
