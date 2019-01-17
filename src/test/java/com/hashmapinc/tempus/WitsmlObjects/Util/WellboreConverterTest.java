046a89919602da12ac818d5e327f463f223ee81b
Randy Pitcher <randypitcherii@gmail.com>
2019-01-17 15:15:15 -0600

    added minimal testing of conversion behavior for wellbore

--- src/test/java/com/hashmapinc/tempus/WitsmlObjects/Util/WellboreConverterTest.java
+++ src/test/java/com/hashmapinc/tempus/WitsmlObjects/Util/WellboreConverterTest.java
@@ -1,18 +1,37 @@
 package com.hashmapinc.tempus.WitsmlObjects.Util;
 
+import com.hashmapinc.tempus.WitsmlObjects.v1311.ObjWellbore;
+import com.hashmapinc.tempus.WitsmlObjects.v20.Wellbore;
 import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class WellboreConverterTest {
     //=========================================================================
     // conversions to 1.3.1.1
     //=========================================================================
     @Test
-    public void shouldConvert1411to1311() {
+    public void shouldConvert1411to1311() throws Exception {
+        String srcXML = TestUtilities.getResourceAsString("wellbore_converter/wellbore1411.xml");
+        com.hashmapinc.tempus.WitsmlObjects.v1411.ObjWellbore src = WitsmlMarshal.deserialize(srcXML, com.hashmapinc.tempus.WitsmlObjects.v1411.ObjWellbore.class);
 
+        // get converted wellbore
+        ObjWellbore dest = WellboreConverter.convertTo1311(src);
+
+        // check fields
+        assertEquals(src.getMd(), dest.getMdCurrent());
     }
 
     @Test
-    public void shouldConvert20to1311() {
+    public void shouldConvert20to1311() throws Exception {
+        String srcXML = TestUtilities.getResourceAsString("wellbore_converter/wellbore20.xml");
+        Wellbore src = WitsmlMarshal.deserialize(srcXML, Wellbore.class);
+
+        // get converted wellbore
+        ObjWellbore dest = WellboreConverter.convertTo1311(src);
+
+        // check fields
+        assertEquals(src.getMd(), dest.getMdCurrent());
 
     }
     //=========================================================================
@@ -22,13 +41,27 @@ public class WellboreConverterTest {
     // conversions to 1.4.1.1
     //=========================================================================
     @Test
-    public void shouldConvert1311to1411() {
+    public void shouldConvert1311to1411() throws Exception {
+        String srcXML = TestUtilities.getResourceAsString("wellbore_converter/wellbore1311.xml");
+        ObjWellbore src = WitsmlMarshal.deserialize(srcXML, ObjWellbore.class);
 
+        // get converted wellbore
+        com.hashmapinc.tempus.WitsmlObjects.v1411.ObjWellbore dest = WellboreConverter.convertTo1411(src);
+
+        // check fields
+        assertEquals(src.getMdCurrent(), dest.getMd());
     }
 
     @Test
-    public void shouldConvert20to1411() {
+    public void shouldConvert20to1411() throws Exception {
+        String srcXML = TestUtilities.getResourceAsString("wellbore_converter/wellbore20.xml");
+        Wellbore src = WitsmlMarshal.deserialize(srcXML, Wellbore.class);
 
+        // get converted wellbore
+        com.hashmapinc.tempus.WitsmlObjects.v1411.ObjWellbore dest = WellboreConverter.convertTo1411(src);
+
+        // check fields
+        assertEquals(src.getMd(), dest.getMd());
     }
     //=========================================================================
 
@@ -37,13 +70,27 @@ public class WellboreConverterTest {
     // conversions to 2.0
     //=========================================================================
     @Test
-    public void shouldConvert1311to20() {
+    public void shouldConvert1311to20() throws Exception {
+        String srcXML = TestUtilities.getResourceAsString("wellbore_converter/wellbore1311.xml");
+        ObjWellbore src = WitsmlMarshal.deserialize(srcXML, ObjWellbore.class);
 
+        // get converted wellbore
+        Wellbore dest = WellboreConverter.convertTo20(src);
+
+        // check fields
+        assertEquals(src.getMdCurrent(), dest.getMd());
     }
 
     @Test
-    public void shouldConvert1411to20() {
+    public void shouldConvert1411to20() throws Exception {
+        String srcXML = TestUtilities.getResourceAsString("wellbore_converter/wellbore1411.xml");
+        com.hashmapinc.tempus.WitsmlObjects.v1411.ObjWellbore src = WitsmlMarshal.deserialize(srcXML, com.hashmapinc.tempus.WitsmlObjects.v1411.ObjWellbore.class);
 
+        // get converted wellbore
+        Wellbore dest = WellboreConverter.convertTo20(src);
+
+        // check fields
+        assertEquals(src.getMd(), dest.getMd());
     }
     //=========================================================================
 }
