6ed5d1469d47c572e558498e5acf17a5cd70e3bd
Randy Pitcher <randypitcherii@gmail.com>
2019-01-21 11:07:16 -0600

    created well converter skeleton class

---
+++ src/main/java/com/hashmapinc/tempus/WitsmlObjects/Util/WellConverter.java
@@ -0,0 +1,19 @@
+package com.hashmapinc.tempus.WitsmlObjects.Util;
+
+/**
+ * This class converts well singular
+ * objects between 1.3.1.1 and 1.4.1.1 objects.
+ *
+ * NOTE: fields are mapped with best effort conversions. Where
+ * direct mapping is not possible, null values are provided.
+ * It is up to the user of this library to use field setters on converted objects to
+ * create valid WITSML objects.
+ */
+public class WellConverter {
+    public com.hashmapinc.tempus.WitsmlObjects.v1311.ObjWell convertTo1311(com.hashmapinc.tempus.WitsmlObjects.v1411.ObjWell src) {
+        return null;
+    }
+    public com.hashmapinc.tempus.WitsmlObjects.v1411.ObjWell convertTo1411(com.hashmapinc.tempus.WitsmlObjects.v1311.ObjWell src) {
+        return null;
+    }
+}
