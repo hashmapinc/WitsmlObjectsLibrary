30f3419700f839ed0bb3df375285c28e72e2c0a2
Randy Pitcher <randypitcherii@gmail.com>
2019-01-24 11:58:31 -0600

    added 1311/1411 conversion basic testing for trajectory conversion

--- src/test/java/com/hashmapinc/tempus/WitsmlObjects/Util/TrajectoryConverterTest.java
+++ src/test/java/com/hashmapinc/tempus/WitsmlObjects/Util/TrajectoryConverterTest.java
@@ -2,12 +2,22 @@ package com.hashmapinc.tempus.WitsmlObjects.Util;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class TrajectoryConverterTest {
     //=========================================================================
     // conversions to 1.3.1.1
     //=========================================================================
     @Test
-    public void shouldConvert1411To1311() {
+    public void shouldConvert1411To1311() throws Exception{
+        String srcXML = TestUtilities.getResourceAsString("trajectory_converter/trajectory1411.xml");
+        com.hashmapinc.tempus.WitsmlObjects.v1411.ObjTrajectory src = ((com.hashmapinc.tempus.WitsmlObjects.v1411.ObjTrajectorys)WitsmlMarshal.deserialize(srcXML, com.hashmapinc.tempus.WitsmlObjects.v1411.ObjTrajectory.class)).getTrajectory().get(0);
+
+        // get converted object
+        com.hashmapinc.tempus.WitsmlObjects.v1311.ObjTrajectory dest = TrajectoryConverter.convertTo1311(src);
+
+        // check fields
+        assertEquals(src.getNameWell(), dest.getNameWell());
     }
 
     @Test
@@ -20,7 +30,15 @@ public class TrajectoryConverterTest {
     // conversions to 1.4.1.1
     //=========================================================================
     @Test
-    public void shouldConvert1311To1411() {
+    public void shouldConvert1311To1411() throws Exception{
+        String srcXML = TestUtilities.getResourceAsString("trajectory_converter/trajectory1311.xml");
+        com.hashmapinc.tempus.WitsmlObjects.v1311.ObjTrajectory src = ((com.hashmapinc.tempus.WitsmlObjects.v1311.ObjTrajectorys)WitsmlMarshal.deserialize(srcXML, com.hashmapinc.tempus.WitsmlObjects.v1311.ObjTrajectory.class)).getTrajectory().get(0);
+
+        // get converted object
+        com.hashmapinc.tempus.WitsmlObjects.v1411.ObjTrajectory dest = TrajectoryConverter.convertTo1411(src);
+
+        // check fields
+        assertEquals(src.getNameWell(), dest.getNameWell());
     }
 
     @Test
