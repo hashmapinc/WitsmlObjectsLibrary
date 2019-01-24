b0b26b6ee0af44b65d80d05db47ee8f5f133bcc5
Randy Pitcher <randypitcherii@gmail.com>
2019-01-24 11:51:17 -0600

    added converter, test suite, and test files for Trajectory conversion

---
+++ src/main/java/com/hashmapinc/tempus/WitsmlObjects/Util/TrajectoryConverter.java
@@ -0,0 +1,41 @@
+package com.hashmapinc.tempus.WitsmlObjects.Util;
+
+public class TrajectoryConverter {
+    //=========================================================================
+    // conversions to 1.3.1.1
+    //=========================================================================
+    public static com.hashmapinc.tempus.WitsmlObjects.v1311.ObjTrajectory convertTo1311(com.hashmapinc.tempus.WitsmlObjects.v1411.ObjTrajectory src) {
+        return null;
+    }
+
+    public static com.hashmapinc.tempus.WitsmlObjects.v1311.ObjTrajectory convertTo1311(com.hashmapinc.tempus.WitsmlObjects.v20.Trajectory src) {
+        return null;
+    }
+    //=========================================================================
+
+
+    //=========================================================================
+    // conversions to 1.4.1.1
+    //=========================================================================
+    public static com.hashmapinc.tempus.WitsmlObjects.v1411.ObjTrajectory convertTo1411(com.hashmapinc.tempus.WitsmlObjects.v1311.ObjTrajectory src) {
+        return null;
+    }
+
+    public static com.hashmapinc.tempus.WitsmlObjects.v1411.ObjTrajectory convertTo1411(com.hashmapinc.tempus.WitsmlObjects.v20.Trajectory src) {
+        return null;
+    }
+    //=========================================================================
+
+
+    //=========================================================================
+    // conversions to 2.0
+    //=========================================================================
+    public static com.hashmapinc.tempus.WitsmlObjects.v20.Trajectory convertTo20(com.hashmapinc.tempus.WitsmlObjects.v1311.ObjTrajectory src) {
+        return null;
+    }
+
+    public static com.hashmapinc.tempus.WitsmlObjects.v20.Trajectory convertTo20(com.hashmapinc.tempus.WitsmlObjects.v1411.ObjTrajectory src) {
+        return null;
+    }
+    //=========================================================================
+}
