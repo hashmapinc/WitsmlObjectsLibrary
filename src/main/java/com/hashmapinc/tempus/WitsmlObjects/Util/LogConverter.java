8446077f87f241ea152422fc960c53702f4252ad
Randy Pitcher <randypitcherii@gmail.com>
2019-01-30 10:37:48 -0600

    added log converter

---
+++ src/main/java/com/hashmapinc/tempus/WitsmlObjects/Util/LogConverter.java
@@ -0,0 +1,41 @@
+package com.hashmapinc.tempus.WitsmlObjects.Util;
+
+public class LogConverter {
+    //=========================================================================
+    // conversions to 1.3.1.1
+    //=========================================================================
+    public static com.hashmapinc.tempus.WitsmlObjects.v1311.ObjLog convertTo1311(com.hashmapinc.tempus.WitsmlObjects.v1411.ObjLog src) {
+        return null;
+    }
+
+    public static com.hashmapinc.tempus.WitsmlObjects.v1311.ObjLog convertTo1311(com.hashmapinc.tempus.WitsmlObjects.v20.Log src) {
+        return null;
+    }
+    //=========================================================================
+
+
+    //=========================================================================
+    // conversions to 1.4.1.1
+    //=========================================================================
+    public static com.hashmapinc.tempus.WitsmlObjects.v1411.ObjLog convertTo1411(com.hashmapinc.tempus.WitsmlObjects.v1311.ObjLog src) {
+        return null;
+    }
+
+    public static com.hashmapinc.tempus.WitsmlObjects.v1411.ObjLog convertTo1411(com.hashmapinc.tempus.WitsmlObjects.v20.Log src) {
+        return null;
+    }
+    //=========================================================================
+
+
+    //=========================================================================
+    // conversions to 2.0
+    //=========================================================================
+    public static com.hashmapinc.tempus.WitsmlObjects.v20.Log convertTo20(com.hashmapinc.tempus.WitsmlObjects.v1311.ObjLog src) {
+        return null;
+    }
+
+    public static com.hashmapinc.tempus.WitsmlObjects.v20.Log convertTo20(com.hashmapinc.tempus.WitsmlObjects.v1411.ObjLog src) {
+        return null;
+    }
+    //=========================================================================
+}
