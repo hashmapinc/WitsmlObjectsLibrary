b329bc30f862095ab564b863873ca23577a8a886
Theresa Stewart <Theresa.Stewart@hashmapinc.com>
2019-08-07 14:55:47 -0500

    Change to Java 11 & fix 2.0 -> 1.4.1.1 Rheometer issue
    Change to Java 11 & fix 2.0 -> 1.4.1.1 Rheometer issue

--- src/main/java/com/hashmapinc/tempus/WitsmlObjects/v20/Fluid.java
+++ src/main/java/com/hashmapinc/tempus/WitsmlObjects/v20/Fluid.java
@@ -2548,7 +2548,7 @@ public class Fluid {
 
         if (this.getSulfide() != null)
             dest.setSulfide(this.getSulfide().to1411GenericMeasure());
-        
+
         if (this.getRheometer() != null){
             for (com.hashmapinc.tempus.WitsmlObjects.v20.Rheometer rheometer : this.getRheometer()){
                 dest.getRheometer().add(rheometer.to1411CsRheometer());
